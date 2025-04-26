6d3020b1d94ac54f12ecd53dbc5f26050fd91e55
Ramiro Mata <rowz.val@gmail.com>
2025-04-25 22:46:42 -0600

    Archivos base

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    
+    return "Testeando la actividad 3"
+
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
